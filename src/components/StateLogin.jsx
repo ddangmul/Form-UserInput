32f6d28cd448ed079ddff0f7582f7793fd34b4bc
ddangmul <j.ddangmul@gmail.com>
2024-12-25 22:39:12 +0900

    커스텀 훅 useInput 생성

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -1,47 +1,63 @@
 import { useState } from "react";
-import Input from "./Input";
+
+import Input from "./Input.jsx";
+import { isEmail, isNotEmpty, hasMinLength } from "../util/validation.js";
+import { useInput } from "../hooks/useInput.js";
 
 export default function Login() {
   // const [enteredEmail, setEnteredEmail] = useState("");
   // const [enteredPassword, setEnteredPassword] = useState("");
-  const [enteredValues, setEnteredValues] = useState({
-    email: "",
-    password: "",
-  });
+  // const [enteredValues, setEnteredValues] = useState({
+  //   email: "",
+  //   password: "",
+  // });
 
-  const [didEdit, setDidEdit] = useState({
-    email: false,
-    password: false,
-  });
+  // const [didEdit, setDidEdit] = useState({
+  //   email: false,
+  //   password: false,
+  // });
 
-  // 이메일 값이 변경되어 컴포넌트가 재실행될 때마다 유효성 검증
-  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
-  const passwordIsInvalid =
-    didEdit.password && enteredValues.password.trim().length < 6;
+  // email
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleInputBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleInputBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    console.log(enteredValues);
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+    console.log(emailValue, passwordValue);
   }
 
-  function handleInputChange(identifier, value) {
-    setEnteredValues((prevValues) => ({
-      ...prevValues,
-      [identifier]: value, // 속성명에 대괄호[] 지정, value 직접적 지정
-    }));
-    setDidEdit((prevDidEdit) => ({
-      ...prevDidEdit,
-      [identifier]: false,
-    }));
-  }
+  // function handleInputChange(identifier, value) {
+  //   setEnteredValues((prevValues) => ({
+  //     ...prevValues,
+  //     [identifier]: value, // 속성명에 대괄호[] 지정, value 직접적 지정
+  //   }));
+  //   setDidEdit((prevDidEdit) => ({
+  //     ...prevDidEdit,
+  //     [identifier]: false,
+  //   }));
+  // }
 
-  function handleInputBlur(identifier) {
-    setDidEdit((prevDidEdit) => ({
-      ...prevDidEdit,
-      [identifier]: true,
-    }));
-  }
+  // function handleInputBlur(identifier) {
+  //   setDidEdit((prevDidEdit) => ({
+  //     ...prevDidEdit,
+  //     [identifier]: true,
+  //   }));
+  // }
 
   return (
     <form onSubmit={handleSubmit}>
@@ -53,23 +69,21 @@ export default function Login() {
           id="email"
           type="email"
           name="email"
-          value={enteredValues.email}
-          onBlur={() => handleInputBlur("email")}
-          onChange={(event) => handleInputChange("email", event.target.value)}
-          error={emailIsInvalid && "Please enter a valid email!"}
+          value={emailValue}
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          error={emailHasError && "Please enter a valid email!"}
         />
 
         <Input
-          label="password"
+          label="Password"
           id="password"
           type="password"
           name="password"
-          onChange={(event) =>
-            handleInputChange("password", event.target.value)
-          }
-          onBlur={() => handleInputBlur("password")}
-          value={enteredValues.password}
-          error={passwordIsInvalid && "Please enter a valid password!"}
+          onChange={handlePasswordChange}
+          onBlur={handlePasswordBlur}
+          value={passwordValue}
+          error={passwordHasError && "Please enter a valid password!"}
         />
       </div>
 
---
+++ src/hooks/useInput.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export function useInput(defaultValue, validationFn) {
+  const [enteredValue, setEnteredValue] = useState(defaultValue);
+  const [didEdit, setDidEdit] = useState(false);
+
+  // 유효성 검사 : 값을 직접 코딩해서 훅에 넣으면 안 됨. 다양한 입력에 사용되어야 하기 때문에 속성값으로 받기
+  const valueIsValid = validationFn(enteredValue);
+
+  function handleInputChange(event) {
+    setEnteredValue(event.target.value);
+    setDidEdit(false);
+  }
+
+  function handleInputBlur() {
+    setDidEdit(true);
+  }
+
+  return {
+    value: enteredValue,
+    handleInputChange,
+    handleInputBlur,
+    hasError: didEdit && !valueIsValid,
+  };
+}
